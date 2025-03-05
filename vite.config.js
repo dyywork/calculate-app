50eb06c6568ed0d6960d82c1fe136c90e5f9bb40
dingyongya <yahya_dyy@163.com>
2025-03-05 10:29:26 +0800

    发布问题

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/calculate-app/', //github pages
+  base: '/calculate-app/', //github pages
   plugins: [vue()],
+
 })
